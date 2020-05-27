f774acfd548a79c3531371c84ffc36fa2dbd70d5
Yanira <yanimaripop@gmail.com>
2020-05-26 22:21:35 -0300

    Birds flying high

--- src/myWindow.py
+++ src/myWindow.py
@@ -32,7 +32,7 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.contador = self.contador + 1
             texto = texto + x + "\n"
             self.label_select.setText(texto)
-        if self.contador < 2:
+        if self.contador < 2:   #chequea que haya al menos dos archivos para unificar
             self.widgetO = QMessageBox(
                 QMessageBox.Warning,
                 "Atención",
@@ -41,35 +41,30 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             )
             self.widgetO.show()
 
-#get_column y get_text funcionan juntas para que el texto que introduce el usuario como columna comun sea key de la unificación posterior
+#get_column y get_text funcionan juntas para que el texto que introduce el usuario como columna común sea index de la unificación posterior
     def get_column(self):
         self.column = self.get_text()
         self.backend()
 
     def get_text(self):
         text = self.lineEdit_cc.text()
-        text_split = text.split(" ")
-        return text_split
+        return text
 
 #Esta función unifica los archivos seleccionados
     def backend(self):
-        for r in self.nombre[0]:
+        for r in self.nombre[0]:    #para cada archivo
             sheet = pd.read_excel(r)
-            y = 0
-            while y < len(self.column):
-                if self.column[y] not in sheet.columns:
-                    self.widgetE = QMessageBox(
-                        QMessageBox.Critical,
-                        "Error",
-                        "La/s columna/s seleccionada/s no se encuentra/n en todos los archivos",
-                        QMessageBox.Ok
-                    )
-                    self.widgetE.show()
-                    return 0
-                y = y + 1
-            for c in self.column:
-                sheet = pd.read_excel(r, index_col=c)
-                sheetdata = sheet.to_dict("index")
+            if self.column not in sheet.columns:    #chequeo si la columna está en la tabla
+                self.widgetE = QMessageBox(
+                    QMessageBox.Critical,
+                    "Error",
+                    "La/s columna/s seleccionada/s no se encuentra/n en todos los archivos",
+                    QMessageBox.Ok
+                )
+                self.widgetE.show()
+                return 0
+            sheet = pd.read_excel(r, index_col=self.column)
+            sheetdata = sheet.to_dict("index")
             for key in sheetdata:
                 if key not in self.archivo:
                     self.archivo[key] = dict()
@@ -78,13 +73,13 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.archivo[key][col] = sheetdata[key][col]
 
         basedatos = pd.DataFrame.from_dict(self.archivo, orient='index')
+        basedatos = basedatos.rename_axis(self.column)
         basedatos = basedatos.fillna("-")
         basedatos.to_excel('listas_unificadas.xlsx')
         self.exito()
-
     """
     Esta función muestra un mensaje de éxito si se logra la unificación y
-    reinicia las variables clave del inicio por si se quiere realizar otra operación
+    reinicializa el diccionario de unificación por si se quiere realizar otra operación
     """
     def exito(self):
         self.widgetInfo = QMessageBox(
@@ -94,4 +89,4 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             QMessageBox.Ok
         )
         self.widgetInfo.show()
-        self.archivo = []
+        self.archivo = dict()
